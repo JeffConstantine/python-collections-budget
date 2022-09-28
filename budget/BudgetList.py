91e7c06a7690ba8c2276078fe57e194b02450d1a
Jeff Constantine <conno80@gmail.com>
2022-09-28 15:35:54 +0800

    Update BudgetList.py
    edit step 2

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -3,10 +3,9 @@ import budget
 
 class BudgetList:
    def __init__(self, budget):
-       ''' self.budget = budget
+       self.budget = budget
         self.sum_expenses = 0
-        self.expenses[]
+        self.expenses = []
         self.sum_overages = 0
-        self.overages[]
+        self.overages = []
 
-'''
