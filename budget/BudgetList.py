b686538a7367ff8381101bf95d134dd35059dc2c
Jeff Constantine <conno80@gmail.com>
2022-09-28 16:10:46 +0800

    Update BudgetList.py
    add step 10

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -27,3 +27,5 @@ def main():
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
+for expense in expenses.list:
+    append.expense.amount(myBudgetList)
